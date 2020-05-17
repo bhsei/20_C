2af63e465999aa17abcc38689138ab2b4b2465a9
Wenbin Zhang <wenbin.zhang97@gmail.com>
2020-05-17 19:16:39 +0800

    remove duplicate files

--- 实验3：软件设计与实现/src/project/routes/index_view.py
+++
@@ -1,9 +0,0 @@
-import flask_login
-from flask import Flask, redirect,render_template,request,Blueprint
-
-index_bp = Blueprint('index', __name__, url_prefix='/')
-
-@index_bp.route('/', methods=['GET', 'POST'])
-def add():
-    print("index")
-    return render_template('home.html', user=flask_login.current_user)
